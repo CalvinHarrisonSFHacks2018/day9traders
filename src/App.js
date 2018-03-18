cc6f75684dd9c7ffb776fe1508f4296a4a334f14
Vaiolis <harrisonllin@gmail.com>
2018-03-17 17:26:27 -0700

    Initial views and models for assets trackers

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+// Import React and Component
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// Import CSS from App.css
 import './App.css';
+// Import the Today component to be used below
+import Today from './Today/Today'
+// Import the History component to be used below
+import History from './History/History'
 
 class App extends Component {
   
-    render() {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+          <div className="topheader">
+              <header className="container">
+                  <nav className="navbar">
+                      <div className="navbar-brand">
+                          <span className="navbar-item">Day9Traders</span>
+                      </div>
+                      <div className="navbar-end">
+                          <a className="navbar-item" href="https://google.com" target="_blank" rel="noopener noreferrer">day9traders.com</a>
+                      </div>
+                  </nav>
+              </header>
+          </div>
+          <section className="results--section">
+              <div className="container">
+                  <h1>Day9Traders is a realtime price information about<br></br> Stocks, Cryptocurrencies, and other assets.</h1>
+              </div>
+              <div className="results--section__inner">
+                  <Today />
+                  <History />
+              </div>
+          </section>
       </div>
     );
   }
