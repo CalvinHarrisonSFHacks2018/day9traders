28eeb05b273522e9b37e930ac5d59594f78afc8d
Calvin Kim <calvinmkim@sbcglobal.net>
2018-03-20 22:23:52 -0700

    OnsenUI - HelloWorld
    Working helloworld example. Working button and alarm using onsenui.
    
    ___________________
    Errors still occuring:
    
    Unexpected token errors for "<"
    
    Uncaught typeerror: Cannot read property keys of undefined
    
    React.createClass is depreciated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import {Page, Button, Toolbar} from 'react-onsenui';
+import {notification} from 'onsenui';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+//var React = require('react'); //duplicate declaration. Not needed
+var ReactDOM = require('react-dom');
+var ons = require('onsenui');
+var Ons = require('react-onsenui');
+
+//document.write('<link rel="stylesheet" href="node_modules/onsenui/css/onsenui.css">');
+
+
+    var App = React.createClass({
+       handleClick: function() {
+        ons.notification.alert('Hello world!');
+      },
+
+      render: function() {
+        return (
+          <Ons.Page>
+            <Ons.Button onClick={this.handleClick}>Tap me!</Ons.Button>
+          </Ons.Page>
+        );
+      }
+    });
+
+    //const app = document.getElementById("root");
+    ReactDOM.render(<App/>, document.getElementById('root'));
+
+
+//var React = require('react'); //Is a duplicate declaration of React
+
+
+
 
 export default App;
